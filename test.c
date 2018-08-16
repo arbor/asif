985e892ff4ac39341aa166deffff2981645af592
David Turnbull <david@broodax.net>
2018-08-16 10:46:33 +1000

    Example module

--- test.c
+++ test.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Include Haskell FFI file, which we will use to initialize a Haskell runtime
+#include "HsFFI.h"
+
+/* #ifdef __GLASGOW_HASKELL__ */
+#include ".stack-work/dist/x86_64-osx/Cabal-2.0.1.0/build/Example_stub.h"
+/* #endif */
+
 int
-main(int argc, char *argv[])
+main( int argc, char *argv[] )
 {
-    printf("hello\n");
-    exit(0);
+  // Initialize Haskell Runtime _before_ any calls to the Haskell code
+  hs_init (&argc, &argv);
+
+  // Make a call to Haskell code
+  entrypoint();
 }
