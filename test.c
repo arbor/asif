57fa937c6febac34116844a8fb1ecb3820ea7bf8
David Turnbull <david@broodax.net>
2018-08-16 11:09:53 +1000

    add the stub file

--- test.c
+++ test.c
@@ -4,9 +4,7 @@
 // Include Haskell FFI file, which we will use to initialize a Haskell runtime
 #include "HsFFI.h"
 
-/* #ifdef __GLASGOW_HASKELL__ */
-#include ".stack-work/dist/x86_64-osx/Cabal-2.0.1.0/build/Example_stub.h"
-/* #endif */
+#include "src/Example_stub.h"
 
 int
 main( int argc, char *argv[] )
