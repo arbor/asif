ebf444c31cde3bbd00db611b108d0213b744f73b
John Ky <newhoggy@gmail.com>
2018-11-05 13:07:55 +1100

    Fix warnings

--- app/App/Commands/DumpOnly.hs
+++ app/App/Commands/DumpOnly.hs
@@ -9,44 +9,24 @@ module App.Commands.DumpOnly
 
 import App.Commands.Options.Type
 import App.Dump
-import Arbor.File.Format.Asif.Data.Ip
 import Arbor.File.Format.Asif.IO
 import Arbor.File.Format.Asif.Segment
-import Arbor.File.Format.Asif.Whatever
 import Control.Lens
 import Control.Monad
-import Control.Monad.IO.Class          (liftIO)
-import Control.Monad.Trans.Resource    (MonadResource, runResourceT)
-import Data.Char                       (isPrint)
+import Control.Monad.IO.Class         (liftIO)
+import Control.Monad.Trans.Resource   (MonadResource, runResourceT)
 import Data.Generics.Product.Any
-import Data.List
 import Data.Maybe
-import Data.Monoid                     ((<>))
-import Data.Text                       (Text)
-import Data.Thyme.Clock
-import Data.Thyme.Clock.POSIX          (POSIXTime)
-import Data.Thyme.Format               (formatTime)
-import Data.Thyme.Time.Core
-import Data.Word
-import HaskellWorks.Data.Bits.BitShow
-import HaskellWorks.Data.Bits.BitWise
-import Numeric                         (showHex)
+import Data.Monoid                    ((<>))
+import Data.Text                      (Text)
 import Options.Applicative
-import System.Locale                   (defaultTimeLocale, iso8601DateFormat)
 
-import qualified Arbor.File.Format.Asif.ByteString.Lazy as LBS
-import qualified Arbor.File.Format.Asif.Format          as F
-import qualified Data.Attoparsec.ByteString             as AP
-import qualified Data.Binary                            as G
-import qualified Data.Binary.Get                        as G
-import qualified Data.Bits                              as B
-import qualified Data.ByteString                        as BS
-import qualified Data.ByteString.Lazy                   as LBS
-import qualified Data.ByteString.Lazy.Char8             as LBSC
-import qualified Data.Set                               as S
-import qualified Data.Text                              as T
-import qualified Data.Text.Encoding                     as T
-import qualified System.IO                              as IO
+import qualified Data.Attoparsec.ByteString as AP
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Lazy       as LBS
+import qualified Data.Set                   as S
+import qualified Data.Text                  as T
+import qualified System.IO                  as IO
 
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
